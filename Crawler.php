36bde2fa27b93b4c22e8e89f7e2580ed9149c07d
mass-min <koyod111@gmail.com>
2020-02-08 22:39:58 +0900

    クローラーClassの各メソッドにコメントを追加

--- Crawler.php
+++ Crawler.php
@@ -4,6 +4,7 @@ require 'vendor/autoload.php';
 
 class Crawler
 {
+    // 目黒区/賃料が安い順/30件ずつ
     const INDEX_URL = 'https://suumo.jp/jj/chintai/ichiran/FR301FC005/?ar=030&bs=040&ta=13&sc=13110&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po2=99&po1=00';
 
     /**
@@ -27,9 +28,11 @@ class Crawler
     {
         self::beforeAction();
 
+        // indexページの総ページ数取得
         $indexPage = file_get_contents(self::INDEX_URL);
         $pageCount = self::getTotalPage($indexPage);
 
+        // 各indexページのhtmlを取得
         for ($pageIndex = 1; $pageIndex < $pageCount; $pageIndex++) {
             $indexPage = file_get_contents(self::INDEX_URL . '&page=' . $pageIndex);
             $file = fopen('tmp/index_' . $pageIndex . '.html', 'w');
@@ -48,6 +51,7 @@ class Crawler
      */
     private function getTotalPage($handle)
     {
+        // ページネーションの最後の数字から総ページ数を取得
         $domIndexPage = phpQuery::newDocument($handle);
         $pageCount = (int)$domIndexPage->find('.pagination-parts > li:last-child a')->text();
         echo 'total page count: ' . $pageCount . "\n";
