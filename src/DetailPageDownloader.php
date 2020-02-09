7324bd4d7e3c9723ecc940c64107443356b110fe
mass-min <koyod111@gmail.com>
2020-02-09 22:39:24 +0900

    詳細ページのHTML格納用ディレクトリを正しく指定し直した

--- src/DetailPageDownloader.php
+++ src/DetailPageDownloader.php
@@ -6,8 +6,9 @@ use SplFileObject;
 
 class DetailPageDownloader
 {
-    const DOWNLOAD_DIR_PATH = 'tmp/detailLinks';
+    const DOWNLOAD_DIR_PATH = 'tmp/detailHtml';
     const SUUMO_BASE_URL = 'https://suumo.jp';
+
     /**
      * @param string $DetailLinksCsvFileName
      * @return void
@@ -24,18 +25,20 @@ class DetailPageDownloader
             echo "Complete to creating tmp directory.\n";
         }
 
+        // CSVファイルの総行数を取得
+        $csvFile->seek(PHP_INT_MAX);
+        $totalCount = $csvFile->key(); // keyは0から始まるがヘッダー行があるので +1 はしない
+        $csvFile->seek(1); // 1行目に移動(ヘッダーの下)
+
         foreach ($csvFile as $index => $line) {
-            if ($index == 0) {
-                continue;
-            }
-            $url = self::SUUMO_BASE_URL . $line;
-            $fileName = self::DOWNLOAD_DIR_PATH . '/detail_' . $line . '.html';
+            $url = self::SUUMO_BASE_URL . $line[0];
+            $fileName = self::DOWNLOAD_DIR_PATH . '/detail_' . $index . '.html';
 
             $pageDownloader = new PageDownloader();
             $pageDownloader->download($url, $fileName);
 
             sleep(1);
-            echo "progress: " . $index . '/' . $csvFile->getMaxLineLen() . "\n";
+            echo "progress: " . ($index + 1) . '/' . $totalCount . "\n";
         }
     }
 }
