d6ff987b38ce710155c93e6fdd237a866af91f72
mass-min <koyod111@gmail.com>
2020-02-11 01:57:32 +0900

    スクレイパーClassのリファクタリング

--- src/DetailPageScraper.php
+++ src/DetailPageScraper.php
@@ -14,38 +14,48 @@ class DetailPageScraper
      */
     public function execute(string $detailHtmlFilePath): void
     {
-        $detailHtml = file_get_contents($detailHtmlFilePath);
-        $doc = new Document();
-        $doc->html($detailHtml);
-
+        $doc = $this->initializeDocumentWithHtml($detailHtmlFilePath);
         $data = [];
-        $data['name'] = trim($doc->find('.section_h1-header-title')->text());
-        // 賃料
-        $data['rent'] = trim($doc->find('.property_view_detail-info-main .property_view_main-emphasis')->text());
-        // 管理費・共益費
-        $data['commonServiceFee'] = trim($doc->find('.property_view_detail-info-main .property_data-body')->text());
+        $targets = self::scrapingTargets();
 
-        // 敷金
-        $data['securityDeposit'] = trim($doc->find('.property_view_detail-info-sub .l-property_data li:first-child .property_data-body span:first-child')->text());
-        // 礼金
-        $data['gratuityFee'] = trim($doc->find('.property_view_detail-info-sub .l-property_data li:first-child .property_data-body span:last-child')->text());
-        // 保証金
-        $data['guaranteeDeposit'] = trim($doc->find('.property_view_detail-info-sub .l-property_data li:nth-child(2) .property_data-body')->text());
-        // 敷引・償却
-        $data['repayment'] = trim($doc->find('.property_view_detail-info-sub .l-property_data li:last-child .property_data-body')->text());
-
-        // 間取り
-        $data['floorPlan'] = trim($doc->find('.property_view_detail--floor_type .l-property_data li:first-child .property_data-body')->text());
-        // 専有面積
-        $data['occupiedArea'] = trim($doc->find('.property_view_detail--floor_type .l-property_data li:nth-child(2) .property_data-body')->text());
-        // 向き
-        $data['direction'] = trim($doc->find('.property_view_detail--floor_type .l-property_data li:nth-child(3) .property_data-body')->text());
-        // 建物種別
-        $data['buildingType'] = trim($doc->find('.property_view_detail--floor_type .l-property_data li:nth-child(4) .property_data-body')->text());
-        // 築年数
-        $data['age'] = trim($doc->find('.property_view_detail--floor_type .l-property_data li:last-child .property_data-body')->text());
-
+        foreach ($targets as $target) {
+            $data[$target['property']] = trim($doc->find($target['selector'])->text());
+        }
         var_dump($data);
+    }
+
+    /**
+     * @param string $htmlFilePath
+     * @return Document
+     */
+    public function initializeDocumentWithHtml(string $htmlFilePath): Document
+    {
+        $html = file_get_contents($htmlFilePath);
+        $doc = new Document();
+        $doc->html($html);
+
+        return $doc;
+    }
+
+    /**
+     * @return array
+     */
+    public static function scrapingTargets(): array
+    {
+        return [
+            ['property' => 'name', 'selector' => '.section_h1-header-title'], // 名前
+            ['property' => 'rent', 'selector' => '.property_view_detail-info-main .property_view_main-emphasis'], // 賃料
+            ['property' => 'commonServiceFee', 'selector' => '.property_view_detail-info-main .property_data-body'], // 管理費・共益費
+            ['property' => 'securityDeposit', 'selector' => '.property_view_detail-info-sub .l-property_data li:first-child .property_data-body span:first-child'], // 敷金
+            ['property' => 'gratuityFee', 'selector' => '.property_view_detail-info-sub .l-property_data li:first-child .property_data-body span:last-child'], // 礼金
+            ['property' => 'guaranteeDeposit', 'selector' => '.property_view_detail-info-sub .l-property_data li:nth-child(2) .property_data-body'], // 保証金
+            ['property' => 'repayment', 'selector' => '.property_view_detail-info-sub .l-property_data li:last-child .property_data-body'], // 敷引・償却
+            ['property' => 'floorPlan', 'selector' => '.property_view_detail--floor_type .l-property_data li:first-child .property_data-body'], // 間取り
+            ['property' => 'occupiedArea', 'selector' => '.property_view_detail--floor_type .l-property_data li:nth-child(2) .property_data-body'], // 専有面積
+            ['property' => 'direction', 'selector' => '.property_view_detail--floor_type .l-property_data li:nth-child(3) .property_data-body'], // 向き
+            ['property' => 'buildingType', 'selector' => '.property_view_detail--floor_type .l-property_data li:nth-child(4) .property_data-body'], // 建物種別
+            ['property' => 'age', 'selector' => '.property_view_detail--floor_type .l-property_data li:last-child .property_data-body'], // 築年数
+        ];
     }
 }
 
