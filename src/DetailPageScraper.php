3cd492fae91b637a41b17fb9581a02ac21ba6b37
mass-min <koyod111@gmail.com>
2020-02-11 14:21:31 +0900

    配列の参照の仕方修正

--- src/DetailPageScraper.php
+++ src/DetailPageScraper.php
@@ -18,8 +18,8 @@ class DetailPageScraper
         $data = [];
         $targets = self::scrapingTargets();
 
-        foreach ($targets as $target) {
-            $data[$target['property']] = trim($doc->find($target['selector'])->text());
+        foreach ($targets as $property => $selector) {
+            $data[$property] = trim($doc->find($selector)->text());
         }
         var_dump($data);
     }
@@ -43,18 +43,18 @@ class DetailPageScraper
     public static function scrapingTargets(): array
     {
         return [
-            ['property' => 'name', 'selector' => '.section_h1-header-title'], // 名前
-            ['property' => 'rent', 'selector' => '.property_view_detail-info-main .property_view_main-emphasis'], // 賃料
-            ['property' => 'commonServiceFee', 'selector' => '.property_view_detail-info-main .property_data-body'], // 管理費・共益費
-            ['property' => 'securityDeposit', 'selector' => '.property_view_detail-info-sub .l-property_data li:first-child .property_data-body span:first-child'], // 敷金
-            ['property' => 'gratuityFee', 'selector' => '.property_view_detail-info-sub .l-property_data li:first-child .property_data-body span:last-child'], // 礼金
-            ['property' => 'guaranteeDeposit', 'selector' => '.property_view_detail-info-sub .l-property_data li:nth-child(2) .property_data-body'], // 保証金
-            ['property' => 'repayment', 'selector' => '.property_view_detail-info-sub .l-property_data li:last-child .property_data-body'], // 敷引・償却
-            ['property' => 'floorPlan', 'selector' => '.property_view_detail--floor_type .l-property_data li:first-child .property_data-body'], // 間取り
-            ['property' => 'occupiedArea', 'selector' => '.property_view_detail--floor_type .l-property_data li:nth-child(2) .property_data-body'], // 専有面積
-            ['property' => 'direction', 'selector' => '.property_view_detail--floor_type .l-property_data li:nth-child(3) .property_data-body'], // 向き
-            ['property' => 'buildingType', 'selector' => '.property_view_detail--floor_type .l-property_data li:nth-child(4) .property_data-body'], // 建物種別
-            ['property' => 'age', 'selector' => '.property_view_detail--floor_type .l-property_data li:last-child .property_data-body'], // 築年数
+            'name' => '.section_h1-header-title', // 名前
+            'rent' => '.property_view_detail-info-main .property_view_main-emphasis', // 賃料
+            'commonServiceFee' => '.property_view_detail-info-main .property_data-body', // 管理費・共益費
+            'securityDeposit' => '.property_view_detail-info-sub .l-property_data li:first-child .property_data-body span:first-child', // 敷金
+            'gratuityFee' => '.property_view_detail-info-sub .l-property_data li:first-child .property_data-body span:last-child', // 礼金
+            'guaranteeDeposit' => '.property_view_detail-info-sub .l-property_data li:nth-child(2) .property_data-body', // 保証金
+            'repayment' => '.property_view_detail-info-sub .l-property_data li:last-child .property_data-body', // 敷引・償却
+            'floorPlan' => '.property_view_detail--floor_type .l-property_data li:first-child .property_data-body', // 間取り
+            'occupiedArea' => '.property_view_detail--floor_type .l-property_data li:nth-child(2) .property_data-body', // 専有面積
+            'direction' => '.property_view_detail--floor_type .l-property_data li:nth-child(3) .property_data-body', // 向き
+            'buildingType' => '.property_view_detail--floor_type .l-property_data li:nth-child(4) .property_data-body', // 建物種別
+            'age' => '.property_view_detail--floor_type .l-property_data li:last-child .property_data-body', // 築年数
         ];
     }
 }
