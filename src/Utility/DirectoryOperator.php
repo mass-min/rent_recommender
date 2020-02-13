800bb503fffd3efb4804f494275806dade2af65d
mass-min <koyod111@gmail.com>
2020-02-13 17:16:29 +0900

    ディレクトリ操作用クラスにディレクトリのゲッターを追加

--- src/Utility/DirectoryOperator.php
+++ src/Utility/DirectoryOperator.php
@@ -2,8 +2,12 @@
 
 namespace RentRecommender\Utility;
 
+use Carbon\Carbon;
+
 class DirectoryOperator
 {
+    const TMP_DIR_PATH = 'tmp';
+
     /**
      * @param string $path
      * @return string
@@ -16,5 +20,41 @@ class DirectoryOperator
             echo "Finish creating $path directory.\n";
         }
         return $path;
+    }
+
+    /**
+     * @param string $date
+     * @return string
+     */
+    public static function getIndexHtmlDirPath(string $date): string
+    {
+        return  self::TMP_DIR_PATH . '/indexHtml/' . self::getDateDirPath($date);
+    }
+
+    /**
+     * @param string $date
+     * @return string
+     */
+    public static function getDetailHtmlDirPath(string $date): string
+    {
+        return self::TMP_DIR_PATH . '/detailHtml/' . self::getDateDirPath($date);
+    }
+
+    /**
+     * @param string $date
+     * @return string
+     */
+    public static function getDetailCsvDirPath(string $date): string
+    {
+        return self::TMP_DIR_PATH . '/detailLinks/' . self::getDateDirPath($date);
+    }
+
+    /**
+     * @param string $date
+     * @return string
+     */
+    public static function getDateDirPath (string $date): string
+    {
+        return Carbon::create($date)->format('Y/m/d');
     }
 }
